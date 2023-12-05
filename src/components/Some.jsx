b80b37c016cd388d283280ce24e5bb0b2324d579
usama ibrahim <usamaibrahimusman@gmail.com>
2023-12-05 21:04:21 +0000

    Update other component

--- src/components/Some.jsx
+++ src/components/Some.jsx
@@ -15,6 +15,7 @@ function ServerOffered() {
             <h3 className='text-3xl mt-5n py-1'>Services I Offer</h3>
             <p className='text-md py-2 leading-6 text-gray-80'>
             As a versatile professional, I specialize in crafting robust software solutions, encompassing web and mobile applications. With expertise in programming and a touch of cybersecurity, I focus on delivering high-quality, secure, and innovative digital products            </p>
+           <p> othello </p>
         </div>
 
         <div className='lg:flex gap-5 '>
